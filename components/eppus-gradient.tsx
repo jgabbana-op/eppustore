5aee253db6669cc515dbf772dc8caaa64ad21536
jgabbana-op <jonatan.duncker@gmail.com>
2023-09-24 19:49:44 +0300

    sandwich changed

--- components/eppus-gradient.tsx
+++ components/eppus-gradient.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image';
 export const EppusGradient = () => {
   return (
     <div className="gradientBrown mt-4 flex h-52 w-full justify-between overflow-hidden rounded-lg bg-eppus-brown text-eppus-yellow-font md:h-96 lg:mt-12">
-      <div className="relative h-full flex-1">
-        <h2 className="absolute left-4 top-8 text-lg uppercase md:left-1/4 md:top-20 md:text-4xl">
+      <div className="relative h-full w-1/2">
+        <h2 className="absolute left-4 top-8 whitespace-nowrap text-lg uppercase md:left-1/4 md:top-20 md:text-4xl">
           Made with <br />
           <span className="ml-2 md:ml-8">diligence</span>
         </h2>
@@ -20,12 +20,12 @@ export const EppusGradient = () => {
       </div>
       <motion.div
         initial={{ y: '100%' }}
-        animate={{ y: 20 }}
+        animate={{ y: 10 }}
         transition={{ duration: 0.5, delay: 0.3, type: 'spring', bounce: 0.25 }}
-        className="relative mr-4 mt-4 flex-1 overflow-hidden md:mr-12 md:mt-20"
+        className="relative -mr-5 w-80 overflow-hidden sm:mr-20 md:mr-0 md:w-1/2 lg:mr-0 lg:w-1/2"
       >
         <Image
-          src="/steakhand.png"
+          src="/handsteak2.png"
           alt="Picture of the author"
           priority={true}
           quality={100}
