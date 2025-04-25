8c19279fcfe43296803bbf3d3705767d9d892a84
jgabbana-op <jonatan.duncker@gmail.com>
2025-04-25 16:00:57 +0300

    remove address

--- app/hotspots/page.tsx
+++ app/hotspots/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <PageWrapper className="m-auto w-11/12 max-w-screen-xl">
       <div className="flex flex-col items-center justify-evenly gap-10 pb-10 pt-10 lg:flex-row">
-        <div className="relative h-72 w-72 bg-[url('/eppustestitalo.png')] bg-contain bg-no-repeat lg:h-96 lg:w-96">
+        {/* <div className="relative h-72 w-72 bg-[url('/eppustestitalo.png')] bg-contain bg-no-repeat lg:h-96 lg:w-96">
           <p className="absolute left-1/2 top-8 -translate-x-1/2 whitespace-nowrap text-center lg:bottom-6">
             Malagankatu 3
             <br /> Helsinki Jätkäsaari
@@ -14,7 +14,7 @@ export default function Home() {
             <br /> <span className="text-eppus-yellow">Fri - Sat: 11:00 - 22:00</span>
             <br /> <span className="text-eppus-yellow">Sun: Closed</span>
           </p>
-        </div>
+        </div> */}
         <div className="relative h-72 w-72 bg-[url('/eppustestitalo.png')] bg-contain bg-no-repeat lg:h-96 lg:w-96">
           <p className="absolute bottom-2 left-1/2 -translate-x-1/2 whitespace-nowrap text-center lg:bottom-6">
             Pursimiehenkatu 5,
